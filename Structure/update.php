ab0f873ba43f1a8f5a95dddc6547cc1bc9db9626
jontorregano <jontorregano@gmail.com>
2017-06-12 23:33:35 -0500

    Began work on attempted to update the image on the page.
    Not done yet, but noImage works, so thats good. Im goin play games. Okay
    bye.

--- Structure/update.php
+++ Structure/update.php
@@ -73,9 +73,7 @@
 
     // check if form was submitted
     if($_POST){
-
         try{
-
             // write update query
             // in this case, it seemed like we have so many fields to pass and
             // it is better to label them and not use question marks
@@ -110,8 +108,69 @@
             $stmt->bindParam('foodImage', $foodImage);
 
             // Execute the query
-            if($stmt->execute()){
+            if($stmt->execute()) {
                 echo "<div class='alert alert-success'>Record was updated.</div>";
+
+                if ($foodImage) {
+
+                    // sha1_file() function is used to make a unique file name
+                    $target_directory = "uploads/";
+                    $target_file = $target_directory . $foodImage;
+                    $file_type = pathinfo($target_file, PATHINFO_EXTENSION);
+
+                    //error message is empty
+                    $file_upload_error_messages = "";
+
+                    // make sure that file is a real image
+                    $check = getimagesize($_FILES["foodImage"]["tmp_name"]);
+                    if ($check !== false) {
+                        // submitted file is an image
+                    } else {
+                        $file_upload_error_messages .= "<div>Submitted file is not an image.</div>";
+                    }
+
+                    // make sure certain file types are allowed
+                    $allowed_file_types = array("jpg", "jpeg", "png", "gif");
+                    if (!in_array($file_type, $allowed_file_types)) {
+                        $file_upload_error_messages .= "<div>Only JPG, JPEG, PNG, GIF files are allowed.</div>";
+                    }
+
+                    // make sure file does not exist
+                    if (file_exists($target_file)) {
+                        $file_upload_error_messages .= "<div>Image already exists. Try to change file name.</div>";
+                    }
+
+                    // make sure submitted file is not too large, can't be larger than 1 MB
+                    if ($_FILES['foodImage']['size'] > (1024000)) {
+                        $file_upload_error_messages .= "<div>Image must be less than 1 MB in size.</div>";
+                    }
+
+                    // make sure the 'uploads' folder exists
+                    // if not, create it
+                    if (!is_dir($target_directory)) {
+                        mkdir($target_directory, 0777, true);
+                    }
+
+                    // if $file_upload_error_messages is still empty
+                    if (empty($file_upload_error_messages)) {
+                        // it means there are no errors, so try to upload the file
+                        if (move_uploaded_file($_FILES["foodImage"]["tmp_name"], $target_file)) {
+                            // it means photo was uploaded
+                        } else {
+                            echo "<div class='alert alert-danger'>";
+                            echo "<div>Unable to upload photo.</div>";
+                            echo "<div>Update the record to upload photo.</div>";
+                            echo "</div>";
+                        }
+                    } // if $file_upload_error_messages is NOT empty
+                    else {
+                        // it means there are some errors, so show them to user
+                        echo "<div class='alert alert-danger'>";
+                        echo "<div>{$file_upload_error_messages}</div>";
+                        echo "<div>Update the record to upload photo.</div>";
+                        echo "</div>";
+                    }
+                }
             }else{
                 echo "<div class='alert alert-danger'>Unable to update record. Please try again.</div>";
             }
@@ -158,10 +217,16 @@
                     </label></td>
             </tr>
             <tr>
+                <td>Size</td>
+                <td><label>
+                        <input type='file' name='foodImage' value="<?php echo htmlspecialchars($foodImage, ENT_QUOTES);  ?>" class='form-control'/>
+                    </label></td>
+            </tr>
+            <tr>
                 <td></td>
                 <td>
                     <input type='submit' value='Save Changes' class='btn btn-primary' />
-                    <a href='read.php' class='btn btn-danger'>Back to read products</a>
+                    <a href='read.php' class='btn btn-danger'>Back to Menu</a>
                 </td>
             </tr>
         </table>
